e5ea1fbe831610f185de4aea9f551418e4539eb3
yu7400ki <yu7400ki@gmail.com>
2023-07-10 23:26:16 +0900

    Add semantic tokens customization to theme

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,23 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      semanticTokens: {
+        sizes: {
+          content: {
+            DEFAULT: { value: "64rem" },
+          },
+        },
+        colors: {
+          text: {
+            DEFAULT: { value: "rgba(8, 18, 26, 0.8)" },
+            description: { value: "rgba(8, 18, 26, 0.6)" },
+            placeholder: { value: "rgba(8, 18, 26, 0.4)" },
+            disabled: { value: "rgba(8, 18, 26, 0.4)" },
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
