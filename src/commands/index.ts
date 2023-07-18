c190749c3dcaf0fca3edf1ccbf40d8de1d3474b3
yu7400ki <yu7400ki@gmail.com>
2023-07-18 10:22:06 +0900

    Refactor function signatures, add return types

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,6 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
-export async function getModels(modelsDir: string) {
+export async function getModels(modelsDir: string): Promise<string[]> {
   return await invoke("get_models", { modelsDir });
 }
 
@@ -8,14 +8,14 @@ export async function loadModel(
   modelsDir: string,
   modelName: string,
   modelType: "bloom" | "gpt2" | "gptj" | "gptneox" | "llama" | "mpt",
-) {
+): Promise<boolean> {
   return await invoke("load_model", { modelsDir, modelName, modelType });
 }
 
-export async function inference(prompt: string) {
+export async function inference(prompt: string): Promise<void> {
   return await invoke("inference", { prompt });
 }
 
-export async function stopInference(isStop: boolean) {
+export async function stopInference(isStop: boolean): Promise<void> {
   return await invoke("stop_inference", { isStop });
 }
