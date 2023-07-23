a9eb4d90b4da8059f4d886fa0cc9a06646a8b047
yu7400ki <yu7400ki@gmail.com>
2023-07-23 16:58:40 +0900

    Add modelSettings field to IConversion interface

--- src/features/chat/types/conversion.ts
+++ src/features/chat/types/conversion.ts
@@ -1,8 +1,10 @@
 import type { IMessage } from "./message";
 import type { IPromptSettings } from "./prompt_settings";
+import type { IModelSettings } from "./model_settings";
 
 export interface IConversion {
   id: string;
+  modelSettings: IModelSettings;
   promptSettings: IPromptSettings;
   messages: IMessage[];
 }
