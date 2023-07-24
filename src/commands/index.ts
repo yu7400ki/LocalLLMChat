dc560321100f0eb56bdb818301cd2194a7effd5e
yu7400ki <yu7400ki@gmail.com>
2023-07-24 22:45:18 +0900

    Refactor loadModel function to accept Architecture enum

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,15 +1,13 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
+import { Architecture } from "@/features/chat/types/architecture";
+
 export async function getModels(): Promise<string[]> {
   return await invoke("get_models");
 }
 
-export async function loadModel(
-  modelsDir: string,
-  modelName: string,
-  modelType: "bloom" | "gpt2" | "gptj" | "gptneox" | "llama" | "mpt",
-): Promise<boolean> {
-  return await invoke("load_model", { modelsDir, modelName, modelType });
+export async function loadModel(modelName: string, modelType: Architecture): Promise<boolean> {
+  return await invoke("load_model", { modelName, modelType });
 }
 
 export async function infer(prompt: string): Promise<void> {
