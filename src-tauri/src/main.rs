8d8d56b815aa1b118bedaa73cbf6bc19797cdd25
1F10220146 <s1f102201469@iniad.org>
2023-07-25 04:02:44 +0900

    Change the directory creation method

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -25,9 +25,7 @@ fn main() {
                 .app_local_data_dir()
                 .ok_or("Failed to get local data dir")?;
             let models_dir = local_data_dir.join("models");
-            if !models_dir.exists() {
-                create_dir(&models_dir).map_err(|e| e.to_string())?;
-            }
+            std::fs::create_dir_all(&models_dir).map_err(|e| format!("Failed to create directory: {}", e))?;
 
             let loaded_model = LoadedModel {
                 name: Mutex::new(None),
