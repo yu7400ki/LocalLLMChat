af7fd7cf504014602f74701b30f0c4d44748d39d
yu7400ki <yu7400ki@gmail.com>
2023-07-24 22:57:02 +0900

    Refactor uuid function

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,20 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
 
-const usedUuids: Map<string, string[]> = new Map();
-
-export const uuid = (namespace?: string): string => {
-  if (typeof namespace === "undefined") {
-    return uuidv4();
-  }
-
-  if (!usedUuids.has(namespace)) {
-    usedUuids.set(namespace, []);
-  }
-  const used = usedUuids.get(namespace)!;
-  let id = uuidv4();
-  while (used.includes(id)) {
-    id = uuidv4();
-  }
-  used.push(id);
-  return id;
+export const uuid = (): string => {
+  return uuidv4();
 };
