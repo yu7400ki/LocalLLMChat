94531913fa814ee331297c28d5fc3763ed6836c4
yu7400ki <yu7400ki@gmail.com>
2023-07-25 21:54:19 +0900

    Add error dialog to Chat component

--- src/features/chat/components/Chat.tsx
+++ src/features/chat/components/Chat.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+import { css } from "@styled-system/css";
+
+import Dialog from "@/components/Dialog";
+
 import { useChat } from "../hooks/useChat";
 import { IConversion } from "../types/conversion";
 import ChatLayout from "./ChatLayout";
@@ -10,12 +15,31 @@ type Props = {
   className?: string;
 };
 
+const ErrorWithIcon: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className={css({ display: "flex", alignItems: "center", gap: 2 })}>
+      <CrossCircledIcon width={"1.5rem"} height={"1.5rem"} className={css({ color: "red.500" })} />
+      {children}
+    </div>
+  );
+};
+
 const Chat: React.FC<Props> = ({ defaultConversion, className }) => {
-  const { inferring, submitMessage, conversion } = useChat(defaultConversion);
+  const { inferring, submitMessage, conversion, error, clearError } = useChat(defaultConversion);
 
   return (
     <ChatLayout className={className} onSubmit={submitMessage} disabled={inferring}>
       <Conversion conversion={conversion} />
+      <Dialog
+        open={error !== null}
+        onClose={clearError}
+        title={<ErrorWithIcon>Inference Error</ErrorWithIcon>}
+        description={error}
+        className={css({
+          w: "90%",
+          maxW: "xl",
+        })}
+      />
     </ChatLayout>
   );
 };
