373fd4707ccb338f0b62011da239509f9b30ed74
yu7400ki <yu7400ki@gmail.com>
2023-07-25 22:02:38 +0900

    Refactor Dialog component by removing unnecessary props and code

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -5,31 +5,19 @@ import dynamic from "next/dynamic";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { css, cx } from "@styled-system/css";
 
-import button from "@/recipes/button";
-
 const Root = dynamic(() => import("@radix-ui/react-dialog").then((mod) => mod.Root), {
   ssr: false,
 });
 
 type Props = {
   open: boolean;
-  closeLabel?: string;
-  onClose: () => void;
   title: React.ReactNode;
   description: React.ReactNode;
   children?: React.ReactNode;
   className?: string;
 };
 
-const Dialog: React.FC<Props> = ({
-  open,
-  closeLabel,
-  onClose,
-  title,
-  description,
-  children,
-  className,
-}) => {
+const Dialog: React.FC<Props> = ({ open, title, description, children, className }) => {
   return (
     <Root open={open}>
       <DialogPrimitive.Portal>
@@ -61,18 +49,6 @@ const Dialog: React.FC<Props> = ({
             {description}
           </DialogPrimitive.Description>
           {children}
-          <div
-            className={css({
-              display: "flex",
-              justifyContent: "flex-end",
-            })}
-          >
-            <DialogPrimitive.Close asChild>
-              <button onClick={onClose} className={button({ visual: "solid" })}>
-                {closeLabel || "閉じる"}
-              </button>
-            </DialogPrimitive.Close>
-          </div>
         </DialogPrimitive.Content>
       </DialogPrimitive.Portal>
     </Root>
