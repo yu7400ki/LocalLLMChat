31f007120589bf3a08e1f5a243835f6f0a65c810
yu7400ki <yu7400ki@gmail.com>
2023-07-28 01:29:06 +0900

    Add stop, re-infer, and continue-infer functionality to useChat hook

--- src/features/chat/hooks/useChat.ts
+++ src/features/chat/hooks/useChat.ts
@@ -4,7 +4,7 @@ import { listen } from "@tauri-apps/api/event";
 
 import { infer, loadModel, stopInference } from "@/commands";
 import { toPrompt } from "@/features/chat/utils/conversionProcessor";
-import { appendBotResponse } from "@/features/chat/utils/conversionProcessor";
+import { appendBotResponse, removeSuffix } from "@/features/chat/utils/conversionProcessor";
 import { conversionStore } from "@/store";
 import { notNull } from "@/utils/evaluate";
 import { uuid } from "@/utils/uuid";
@@ -156,11 +156,46 @@ export const useChat = (defaultConversion: IConversion) => {
     setError(null);
   };
 
+  const stop = (force?: boolean) => {
+    console.log("stop");
+    if (force) {
+      stopInference(true);
+      setInferring(false);
+      setBuffer("");
+    } else {
+      stopInference(false);
+    }
+  };
+
+  const reInfer = () => {
+    console.log("re-infer");
+    stop();
+    setConversion((prevConversion) => {
+      prevConversion.messages.pop();
+      return prevConversion;
+    });
+    const prompt = toPrompt(conversion, true);
+    console.log(prompt);
+    inferStart(prompt);
+  };
+
+  const continueInfer = () => {
+    console.log("continue infer");
+    stop();
+    let prompt = toPrompt(conversion, false);
+    prompt = removeSuffix(prompt, conversion.promptSettings.botSuffix);
+    console.log(prompt);
+    inferStart(prompt);
+  };
+
   return {
     conversion,
     inferring,
     submitMessage,
     error,
     clearError,
+    stop,
+    reInfer,
+    continueInfer,
   };
 };
