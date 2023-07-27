d1c6d88135240e58722dd30236e0baf89bbd5872
yu7400ki <yu7400ki@gmail.com>
2023-07-28 01:00:59 +0900

    Refactor MessageInput component to include a children prop

--- src/features/chat/components/MessageInput.tsx
+++ src/features/chat/components/MessageInput.tsx
@@ -8,9 +8,10 @@ type Props = {
   onSubmit: (message: string) => void;
   disabled?: boolean;
   className?: string;
+  children?: React.ReactNode;
 };
 
-const MessageInput: React.FC<Props> = ({ onSubmit, className, disabled }) => {
+const MessageInput: React.FC<Props> = ({ onSubmit, className, disabled, children }) => {
   const [message, setMessage] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -61,6 +62,13 @@ const MessageInput: React.FC<Props> = ({ onSubmit, className, disabled }) => {
       className={cx(
         className,
         css({
+          position: "relative",
+        }),
+      )}
+    >
+      {children}
+      <div
+        className={css({
           display: "flex",
           alignItems: "flex-end",
           borderColor: "slate.200",
@@ -68,77 +76,77 @@ const MessageInput: React.FC<Props> = ({ onSubmit, className, disabled }) => {
           rounded: "xl",
           shadow: "md",
           fontWeight: "semibold",
-        }),
-      )}
-    >
-      <textarea
-        placeholder="メッセージを入力"
-        className={css({
-          boxSizing: "content-box",
-          flex: 1,
-          p: 4,
-          pr: 0,
-          minHeight: "calc(1rem * 1.5)",
-          height: "calc(1rem * 1.5)",
-          maxHeight: "10rem",
-          outline: "none",
-          resize: "none",
-          fontSize: "1rem",
-          lineHeight: 1.5,
-          color: "text",
-          bg: "transparent",
-          _placeholder: {
-            color: "text.placeholder",
-          },
-        })}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        value={message}
-        ref={textareaRef}
-      />
-      <div
-        className={css({
-          height: "3.5rem",
-          display: "grid",
-          placeItems: "center",
-          aspectRatio: 1,
         })}
       >
-        <button
-          disabled={isDisabled}
-          onClick={handleClick}
+        <textarea
+          placeholder="メッセージを入力"
           className={css({
-            p: 2,
-            cursor: "pointer",
-            bg: {
-              base: "orange.500",
-              _hover: "orange.600",
-              _active: "orange.600",
-            },
-            color: "white",
-            rounded: "md",
-            aspectRatio: 1,
+            boxSizing: "content-box",
+            flex: 1,
+            p: 4,
+            pr: 0,
+            minHeight: "calc(1rem * 1.5)",
+            height: "calc(1rem * 1.5)",
+            maxHeight: "10rem",
             outline: "none",
-            transition: "color 150ms ease-in-out, background-color 150ms ease-in-out",
-            _focusVisible: {
-              outlineColor: "sky.600",
-              outlineStyle: "solid",
-              outlineWidth: "2px",
-              outlineOffset: "1px",
-            },
-            _disabled: {
-              bg: {
-                base: "transparent",
-                _hover: "transparent",
-                _active: "transparent",
-              },
-              color: "slate.400",
-              cursor: "not-allowed",
+            resize: "none",
+            fontSize: "1rem",
+            lineHeight: 1.5,
+            color: "text",
+            bg: "transparent",
+            _placeholder: {
+              color: "text.placeholder",
             },
           })}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          value={message}
+          ref={textareaRef}
+        />
+        <div
+          className={css({
+            height: "3.5rem",
+            display: "grid",
+            placeItems: "center",
+            aspectRatio: 1,
+          })}
         >
-          <PaperPlaneIcon stroke="white" strokeWidth={0.4} width={"1.1rem"} height={"1.1rem"} />
-        </button>
+          <button
+            disabled={isDisabled}
+            onClick={handleClick}
+            className={css({
+              p: 2,
+              cursor: "pointer",
+              bg: {
+                base: "orange.500",
+                _hover: "orange.600",
+                _active: "orange.600",
+              },
+              color: "white",
+              rounded: "md",
+              aspectRatio: 1,
+              outline: "none",
+              transition: "color 150ms ease-in-out, background-color 150ms ease-in-out",
+              _focusVisible: {
+                outlineColor: "sky.600",
+                outlineStyle: "solid",
+                outlineWidth: "2px",
+                outlineOffset: "1px",
+              },
+              _disabled: {
+                bg: {
+                  base: "transparent",
+                  _hover: "transparent",
+                  _active: "transparent",
+                },
+                color: "slate.400",
+                cursor: "not-allowed",
+              },
+            })}
+          >
+            <PaperPlaneIcon stroke="white" strokeWidth={0.4} width={"1.1rem"} height={"1.1rem"} />
+          </button>
+        </div>
       </div>
     </div>
   );
